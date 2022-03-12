cb7999b3019d4b2a2318a342708773194cda29c5
nitish velu <nitishvelu@gmail.com>
2022-03-12 21:39:10 +0530

    prisma adapter for next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
 export default NextAuth({
   // Configure one or more authentication providers
