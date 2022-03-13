7115c98780c9252a0e40259608e80e4b46cdc48a
nitish velu <nitishvelu@gmail.com>
2022-03-13 16:14:55 +0530

    remove css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import { ChakraProvider } from '@chakra-ui/react'
+
+
 
 export default function App({
   Component,
@@ -7,7 +9,9 @@ export default function App({
 }) {
   return (
     <SessionProvider session={session}>
+      <ChakraProvider resetCSS>
       <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   )
 }
