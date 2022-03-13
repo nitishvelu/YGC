c32f3131320278db33fcf4026c70ad53657c97dd
nitish velu <nitishvelu@gmail.com>
2022-03-13 16:37:45 +0530

    login page template

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
+
 
 export default function Component() {
   const { data: session } = useSession()
@@ -7,13 +16,54 @@ export default function Component() {
       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
+
       </>
-    )
-  }
+      )}
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+         <>
+         <Container maxW={'3xl'}>
+           <Stack
+             as={Box}
+             textAlign={'center'}
+             spacing={{ base: 8, md: 14 }}
+             py={{ base: 20, md: 36 }}>
+             <Heading
+               fontWeight={600}
+               fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+               lineHeight={'110%'}>
+               Velu<br />
+               <Text as={'span'} color={'green.400'}>
+                 Lends
+               </Text>
+             </Heading>
+             <Text color={'gray.500'}>
+               Web application to keep track of your lending activities and make 
+               it easier to manage your business. By keeping track of all the users and
+               their progress in repayment
+             </Text>
+             <Stack
+               direction={'column'}
+               spacing={3}
+               align={'center'}
+               alignSelf={'center'}
+               position={'relative'}>
+               <Button
+                 colorScheme={'green'}
+                 bg={'green.400'}
+                 rounded={'full'}
+                 onClick={() => signIn()}
+                 px={6}
+                 _hover={{
+                   bg: 'green.500',
+                 }}>
+                 Login
+               </Button>
+             </Stack>
+           </Stack>
+         </Container>
+       </>
+    )
+  
+  
 }
