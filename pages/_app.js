c4d4989fc902fb62f2a98d4ca857d50f657c0ede
nitish velu <nitishvelu@gmail.com>
2022-03-15 10:32:47 +0530

    progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Auth from "../components/Auth";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps, session }) {
   return (
     <ChakraProvider>
+      <NextNProgress />
       <Auth {...pageProps} session={session} Component={Component} />
     </ChakraProvider>
   );
