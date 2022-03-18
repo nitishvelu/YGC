d3e4b13ed6a9fbeddaf22dbced94e22e89234380
nitish velu <nitishvelu@gmail.com>
2022-03-18 19:33:36 +0530

    formik validation

---
+++ pages/api/pay.js
@@ -0,0 +1,47 @@
+import { getSession } from "next-auth/client";
+import prisma from "../../prisma";
+
+// import { PostSchema } from "../../components/PostForm";
+
+async function pay(req, res) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return res.status(401).json({ unauthorized: true });
+  }
+
+  const sessionRecord = await prisma.session.findUnique({
+    where: { accessToken: session.accessToken },
+  });
+
+  const user = await prisma.user.findUnique({
+    where: { id: sessionRecord.userId },
+  });
+
+  //   const valid = await PostSchema.isValid(req.body);
+
+  //   if (!valid) {
+  //     return res.status(500).json({ error: "validation error" });
+  //   }
+  const post = await prisma.post.create({
+    data: {
+      userId: user.id,
+      name: req.body.name,
+      tenure: Number(req.body.tenure),
+      amount: Number(req.body.amount),
+      phone: req.body.phone,
+    },
+  });
+
+  if (post.id) {
+    res.status(200).json(post);
+  } else {
+    return res.status(500).json({ error: "something went wrong" });
+  }
+}
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return pay(req, res);
+  }
+}
