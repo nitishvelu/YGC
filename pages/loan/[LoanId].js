adb8f6505ce69f8d89a9feb16d766fe68de53755
nitish velu <nitishvelu@gmail.com>
2022-03-27 22:25:06 +0530

    order by created date

--- pages/loan/[LoanId].js
+++ pages/loan/[LoanId].js
@@ -1,6 +1,6 @@
 import React from "react";
 import prisma from "../../prisma";
-import { Button } from "@chakra-ui/react";
+import { Button, Flex, Box, Heading, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 function LoanDetails({ user, transactions }) {
@@ -8,21 +8,44 @@ function LoanDetails({ user, transactions }) {
   console.log(transactions);
   console.log(user);
   return (
-    <>
-      <div>{user.name}</div>
-      <Button
-        onClick={() => {
-          router.push("/pay/[payId]", `/pay/${user.id}`);
-        }}
-        color="white"
-        bg="green.300"
-        colorScheme="green"
-        display={"inline"}
-        shadow="base"
+    <Flex flexDir="column" w="100vw" alignItems={"center"}>
+      <Box
+        mx="10"
+        my="1"
+        px="5"
+        py="10"
+        w={["90vw", "80vw", "40vw"]}
+        rounded="lg"
+        shadow={"lg"}
+        justifyContent={"center"}
+        bg="gray.100"
+        h="90vh"
       >
-        Pay
-      </Button>
-    </>
+        <Box display={"inline-block"} mb={2}>
+          <Heading display={"inline"} mr={["30", "300", "470"]}>
+            {user.name}
+          </Heading>
+          <Button
+            onClick={() => {
+              router.push("/pay/[payId]", `/pay/${user.id}`);
+            }}
+            color="white"
+            bg="green.300"
+            colorScheme="green"
+            display={"inline"}
+            shadow="base"
+          >
+            Pay +
+          </Button>
+        </Box>
+        <VStack h="95%" spacing={3} overflowY="auto">
+          {transactions?.map((transaction) => (
+            <Heading key={transaction.id}>{transaction.amount}</Heading>
+          ))}
+          ;
+        </VStack>
+      </Box>
+    </Flex>
   );
 }
 
@@ -38,6 +61,9 @@ export async function getServerSideProps(context) {
   const transactions = await prisma.payment.findMany({
     where: {
       postId: params.LoanId,
+    },
+    orderBy: {
+      createdAt: "desc",
     },
   });
   return {
