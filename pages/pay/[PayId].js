8dd8d2261138b62dac150a15bf9ad355d19aded7
nitish velu <nitishvelu@gmail.com>
2022-04-03 13:17:48 +0530

    pwa setup

--- pages/pay/[PayId].js
+++ pages/pay/[PayId].js
@@ -100,3 +100,30 @@ export default function Pay() {
     </Flex>
   );
 }
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  // map data to an array of path objects with params (id)
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
